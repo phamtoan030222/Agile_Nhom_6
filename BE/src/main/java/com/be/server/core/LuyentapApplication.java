0ae18f3c649ae14b606d26e8dcd94082f69dae53
Hoàng Vũ <tranhoangvu.2711@gmail.com>
2025-12-05 05:59:18 +0700

    Add files via upload
    def

---
+++ BE/src/main/java/com/be/server/core/LuyentapApplication.java
@@ -0,0 +1,13 @@
+package com.example.luyentap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuyentapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuyentapApplication.class, args);
+    }
+
+}
